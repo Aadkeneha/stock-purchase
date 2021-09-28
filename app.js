533084941cc85523a5c478dddabdf4d6e28427b1
Neha Aadke <aadkeneha@gmail.com>
2021-09-28 22:50:01 +0530

    feat:updated code

--- app.js
+++ app.js
@@ -2,20 +2,26 @@ const inputUser = document.querySelectorAll(".input-user");
 const btnSubmit = document.querySelector("#btn-submit");
 const outputelement = document.querySelector("#output");
 
-btnSubmit.addEventListener("click",function calculate(){
+btnSubmit.addEventListener("click", function calculate() {
 
-    const prevalue = Number(inputUser[0].value)*Number(inputUser[1].value);
+    if (Number(inputUser[0].value) === 0 || Number(inputUser[1].value) === 0 || Number(inputUser[2].value) === 0) {
+        outputelement.innerText = "Please enter Valid input";
+    } else {
 
-    const currvalue =Number(inputUser[2].value)*Number(inputUser[1].value);
+        const prevalue = Number(inputUser[0].value) * Number(inputUser[1].value);
 
-    const result = currvalue - prevalue;
-    const percent = result/Number(inputUser[1].value)
+        const currvalue = Number(inputUser[2].value) * Number(inputUser[1].value);
 
-    if(result>0){
-        outputelement.innerText="Yippee!! You gained profit of Rs."+result+ " and your profit percentage is " + percent +"%";
+        const result = currvalue - prevalue;
+        const percent = result / Number(inputUser[1].value)
+
+        if (result > 0) {
+            outputelement.innerText = "Yippee!! You gained profit of Rs." + result + " and your profit percentage is " + percent + "%";
+        } else {
+            outputelement.innerText = "Oops!! You gained loss of Rs." + Math.abs(result) + " and your loss percentage is " + Math.abs(percent) + "%";
+        }
+
     }
-    else{
-        outputelement.innerText="Oops!! You gained loss of Rs."+Math.abs(result)+ " and your loss percentage is " + Math.abs(percent) +"%";
-    }
+
 
 });
