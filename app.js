e90d04c99c3d8da198c6bd4d3c33d57be8045180
Neha Aadke <aadkeneha@gmail.com>
2021-09-28 23:11:00 +0530

    feat: updated code

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const outputelement = document.querySelector("#output");
 btnSubmit.addEventListener("click", function calculate() {
 
     if (Number(inputUser[0].value) === 0 || Number(inputUser[1].value) === 0 || Number(inputUser[2].value) === 0) {
-        outputelement.innerText = "Please enter Valid input";
+        outputelement.innerText = "Please enter values in all input fields";
     } else {
 
         const prevalue = Number(inputUser[0].value) * Number(inputUser[1].value);
@@ -13,12 +13,20 @@ btnSubmit.addEventListener("click", function calculate() {
         const currvalue = Number(inputUser[2].value) * Number(inputUser[1].value);
 
         const result = currvalue - prevalue;
-        const percent = result / Number(inputUser[1].value)
+        const percent = (Math.abs(result)/inputUser[0].value) *100;
 
         if (result > 0) {
-            outputelement.innerText = "Yippee!! You gained profit of Rs." + result + " and your profit percentage is " + percent + "%";
-        } else {
-            outputelement.innerText = "Oops!! You gained loss of Rs." + Math.abs(result) + " and your loss percentage is " + Math.abs(percent) + "%";
+            outputelement.innerText = "Yippee!! You gained profit of Rs." + result + " and your profit percentage is " + percent + "%  🥳";
+        } 
+        else {
+            if(result < 0){
+
+                outputelement.innerText = "Oops!! You gained loss of Rs." + Math.abs(result) + " and your loss percentage is " + Math.abs(percent) + "%  😞";
+            }
+            else{
+                outputelement.innerText = "No Profit, No Loss.So, don't worry,Be Happy 😉";
+            }
+            
         }
 
     }
